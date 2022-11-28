e32150602334c14bf3cdf03bd15836588effd395
Rhema7776 <rhemachukwumah7@gmail.com>
2022-11-28 21:15:00 +0100

    ignored env and added slug

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -10,3 +12,7 @@ urlpatterns = [
     # path('api/blog/', include('blog.urls', namespace='blog'))
     path('api/', include('posts.urls', namespace='posts'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ frontend/src/components/Message.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Message } from 'semantic-ui-react'
+
+export default ({ message,info, positive, warning, negative }) => (
+  <Message  info positive warning negative>
+    {message}
+  </Message>
+)
+
+
